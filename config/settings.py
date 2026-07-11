90c5784600dd2aa5c90aa56cc93341681da1b3d8
Viktor <klarkkent0314@gmail.com>
2026-07-11 22:36:32 +0300

    add urls

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ SIMPLE_JWT = {
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Habit Tracker API",
+    "TITLE": "Employee Task Tracker API",
     "DESCRIPTION": "Backend SPA трекера привычек (Atomic Habits)",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,37 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+    # JWT
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
+    # API основных приложений
+    path("api/", include("users.urls")),
+    path("api/", include("employee_tasks.urls")),
+
+    # OpenAPI схема
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    
+    # Redoc (альтернатива)
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
