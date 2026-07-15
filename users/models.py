39f33a19faebab1789fe954926d4cbe7a3c89dfa
Viktor <klarkkent0314@gmail.com>
2026-07-15 20:56:06 +0300

    small fix

--- users/models.py
+++ users/models.py
@@ -54,6 +54,8 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ["full_name"]
 
     class Meta:
+        verbose_name = ("Сотрудник")
+        verbose_name_plural = ("Сотрудники")
         ordering = ["full_name"]
 
     def __str__(self):
